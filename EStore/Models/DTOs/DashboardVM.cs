f105f5622b73874ad7a4b4694a492eae2e7ca56f
HuaTienHao <huatienhao095@gmail.com>
2023-11-26 14:57:46 +0700

    Added Admin dashboard page (UI only)

---
+++ EStore/Models/DTOs/DashboardVM.cs
@@ -0,0 +1,8 @@
+﻿namespace EStore.Models.DTOs
+{
+    public class DashboardVM
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
