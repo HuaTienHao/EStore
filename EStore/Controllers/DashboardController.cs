bb0050baa054c3f486b6d0aff00c314741c63e74
beelyya <huonghtq15@gmail.com>
2023-12-04 21:41:53 +0700

    -

--- EStore/Controllers/DashboardController.cs
+++ EStore/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
 ﻿using EStore.Models;
+using Humanizer.Localisation;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,35 +11,71 @@ namespace EStore.Controllers
     [Authorize(Roles = "Admin")]
     public class DashboardController : Controller
     {
-        private readonly ApplicationDbContext _context;
-        public DashboardController(ApplicationDbContext context)
+        private readonly IGenreService service;
+        public DashboardController(IGenreService service)
         {
-            _context = context;
+            this.service = service;
         }
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public IActionResult Add()
         {
-            IEnumerable<Category> categories = await _context.Categories.ToListAsync();
-			IEnumerable<Product> products = await (from product in _context.Products
-                        join category in _context.Categories
-                        on product.CategoryId equals category.Id
-                        select new Product
-                        {
-							Id = product.Id,
-							Image = product.Image,
-							StoreName = product.StoreName,
-							ProductName = product.ProductName,
-							CategoryId = product.CategoryId,
-							Price = product.Price,
-							CategoryName = category.CategoryName,
-						}).ToListAsync();
-
-            DashboardVM dashboardVM = new DashboardVM 
-            { 
-                Products = products,
-                Categories = categories
-            };
-            return View(dashboardVM);
+            return View();
         }
+
+        [HttpPost]
+        public IActionResult Add(Category model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var result = service.Add(model);
+            if (result)
+            {
+                TempData["msg"] = "Added Successfully";
+                return RedirectToAction("GetAll");
+            }
+            TempData["msg"] = "Error has occured on server side";
+            return View(model);
+        }
+
+
+        public IActionResult Update(int id)
+        {
+            var record = service.FindById(id);
+            return View(record);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Category model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var result = service.Update(model);
+            if (result)
+            {
+                return RedirectToAction("GetAll");
+            }
+            TempData["msg"] = "Error has occured on server side";
+            return View(model);
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+
+            var result = service.Delete(id);
+            return RedirectToAction("GetAll");
+        }
+
+        public IActionResult GetAll()
+        {
+
+            var data = service.GetAll();
+            return View(data);
+        }
+
     }
 }
